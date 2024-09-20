f5accf5d45710ea0907860eaae31d231c8c047ec
DurionHelenkeller <128843745+DurionHelenkeller@users.noreply.github.com>
2024-09-20 11:23:40 -0600

    Update making_pizzas.py

--- making_pizzas.py
+++ making_pizzas.py
@@ -1,5 +1,4 @@
-def make_pizza(size, *toppings):
-    """Summarize the pizza we are about to make."""
-    print(f"\nMaking a {size}-inch pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
+import pizza
+
+pizza.make_pizza(12, "cheese")
+pizza.make_pizza(16, "pepperoni", "hawaiian", "mushroom")
